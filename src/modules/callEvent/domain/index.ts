a41a80643991332d73b69d6868b940c4634a9f00
DIEGOHORVATTI <d.horvattid@gmail.com>
2025-01-21 14:32:14 -0300

    Enhance CallEvent schema and validation: add new fields for call management and update descriptions

--- src/modules/callEvent/domain/index.ts
+++ src/modules/callEvent/domain/index.ts
@@ -8,34 +8,159 @@ import { CallEventValidation } from './validation'
 
 export type ICallEvent = typeof CallEventValidation.composition.static
 
+export const hookBy = ['A', 'B', 'I']
+export const callbackRegState = [
+  'REGISTERED',
+  'REINCIDENT',
+  'SCHEDULED',
+  'FINAL_NOT_SUCCESS',
+  'FINAL_SUCCESS',
+  'ERROR',
+  'FINAL_HANDLED_REINCIDENT'
+]
+export const endReason = [
+  'FINISHED_HANDLED',
+  'OUT_OF_OPERATION',
+  'ABANDONED',
+  'OVERFLOWED',
+  'TRANSFERRED',
+  'DEVIATED_FROM_QUEUE',
+  'DESTINATION_BUSY',
+  'DESTINATION_NO_ANSWER',
+  'FAIL',
+  'FINISHED_RETENTION_TIMEOUT'
+]
+export const causedBy = [
+  'IN_QUEUE',
+  'IN_DEVICE',
+  'BEFORE_QUEUE',
+  'IN_GENERATION',
+  'INVALID_NUMBER',
+  'NOT_HANDLED_BY_AGENT',
+  'GENERIC',
+  'TO_AGENT',
+  'TO_QUEUE',
+  'TO_DEVICE',
+  'BY_ORIGIN',
+  'TO_QUEUE_BY_RETENTION_TIMEOUT',
+  'HSM_SEND_FAILURE'
+]
+
 const schema = new Schema<ICallEvent, Model<ICallEvent>>(
   {
-    callId: {
+    callId: { type: String, required: true, description: 'callId do Neo para a chamada' },
+    callIdHold: { type: String, description: 'callId da chamada retida - usado quando transferência sob consulta' },
+    originalCallId: {
       type: String,
-      required: true
+      description: 'callId da chamada original - usado em transferência para poder relacionar os atendimentos'
     },
-    timestamp: {
-      type: Date,
-      required: true
-    },
-    status: {
+    serviceId: { type: String, description: 'id do serviço associado à chamada' },
+    externalCallId: {
       type: String,
-      required: true
+      description: 'identificador da chamada utilizado por processo externo ao Neo (ex. chave do PABX)'
     },
-    caller: {
-      id: String,
-      name: String,
-      phoneNumber: String
+    media: {
+      type: {
+        type: String,
+        required: true,
+        description: 'Tipo da mídia'
+      },
+      submedia: {
+        type: String,
+        required: true,
+        description: 'Subtipo da mídia'
+      }
     },
-    agent: {
-      id: String,
-      name: String
+    expectedServiceTime: { type: Number, description: 'tempo esperado de atendimento em segundos' },
+    interlocutor: {
+      type: {
+        type: String,
+        required: true,
+        description: 'Tipo de interlocutor'
+      },
+      id: {
+        type: String,
+        required: true,
+        description: 'Identificador do interlocutor'
+      },
+      flgSource: {
+        type: Boolean,
+        required: true,
+        description: 'Indica se o interlocutor é a fonte principal'
+      },
+      identity: {
+        type: String,
+        required: true,
+        description: 'Identidade do interlocutor'
+      },
+      chatIdentity: {
+        type: String,
+        required: true,
+        description: 'Identidade do chat do interlocutor'
+      }
     },
-    duration: {
-      type: Number
+    attendant: {
+      type: {
+        type: String,
+        required: true,
+        description: 'Tipo do atendente'
+      },
+      id: {
+        type: String,
+        required: true,
+        description: 'Identificador do atendente'
+      },
+      flgSource: {
+        type: Boolean,
+        required: true,
+        description: 'Indica se o atendente é a fonte principal'
+      },
+      identity: {
+        type: String,
+        required: true,
+        description: 'Identidade do atendente'
+      }
     },
-    notes: {
-      type: String
+    flgConsult: { type: Boolean, default: false, description: 'indica se a interação é uma consulta' },
+    flgIncoming: {
+      type: Boolean,
+      required: true,
+      default: true,
+      description: 'indica a direção, true é Entrante, false é Sainte'
+    },
+    associatedData: { type: String, description: 'dados associados da chamada' },
+    protocol: { type: String, description: 'Protocolo da chamada' },
+    callbackId: {
+      type: String,
+      description: 'id do contato do callback, indica que foi uma chamada gerada pelo callback'
+    },
+    flgMonitoring: {
+      type: String,
+      description: 'indica se a chamada está sendo monitorada enviando qual ramal está sendo monitorado'
+    },
+    queuePosition: {
+      type: Number,
+      description: 'registra a posição que o interlocutor estava na fila (abandono, transbordo)'
+    },
+    fidelization: { type: Boolean, description: 'indica se a chamada é de fidelização' },
+    flgPickUp: { type: Boolean, description: 'indica se o agente atendeu a chamada' },
+    flgRecord: { type: Boolean, description: 'indica se a interação foi gravada' },
+    tokenAi: { type: String, description: 'uuid com token referente à integração com IA no atendimento' },
+    hookBy: { type: String, enum: hookBy, description: 'quem desligou a chamada (A, B ou I)' },
+    callbackRegState: {
+      type: String,
+      enum: callbackRegState,
+      description: 'status do registro do callback'
+    },
+    endReason: {
+      type: String,
+      enum: endReason,
+      description: 'condição de término da interação'
+    },
+    causedBy: {
+      type: String,
+      enum: causedBy,
+      description: 'causa da condição de término da interação'
     }
   } satisfies SchemaFactoryProps<ICallEvent>,
   {
@@ -49,3 +174,5 @@ export const CallEvent = schemaFactory<ICallEvent, {}, typeof CallEventValidatio
   name: collectionsData.CallEvent.name,
   validation: CallEventValidation
 })
+
+const lapa: ICallEvent['causedBy'] = 'GENERIC'
--- src/modules/callEvent/domain/validation.ts
+++ src/modules/callEvent/domain/validation.ts
@@ -1,36 +1,155 @@
 import { t as Type } from 'elysia'
-
-const callStatus = {
-  missed: 'missed',
-  received: 'received',
-  rejected: 'rejected'
-}
+import { callbackRegState, causedBy, endReason, hookBy } from '.'
 
 export const schema = {
   callId: Type.String({
     required: true,
-    index: true
+    description: 'callId do Neo para a chamada'
   }),
-  timestamp: Type.Date({
+  callIdHold: Type.String({
+    description: 'callId da chamada retida - usado quando transferência sob consulta'
+  }),
+  originalCallId: Type.String({
+    description: 'callId da chamada original - usado em transferência para poder relacionar os atendimentos'
+  }),
+  serviceId: Type.String({
+    description: 'id do serviço associado à chamada'
+  }),
+  externalCallId: Type.String({
+    description: 'identificador da chamada utilizado por processo externo ao Neo (ex. chave do PABX)'
+  }),
+  media: Type.Object(
+    {
+      type: Type.String({
+        required: true,
+        description: 'Tipo da mídia'
+      }),
+      submedia: Type.String({
+        required: true,
+        description: 'Subtipo da mídia'
+      })
+    },
+    { description: 'tipo da mídia e submídia' }
+  ),
+  expectedServiceTime: Type.Number({
+    description: 'tempo esperado de atendimento em segundos'
+  }),
+  interlocutor: Type.Object(
+    {
+      type: Type.String({
+        required: true,
+        description: 'Tipo de interlocutor'
+      }),
+      id: Type.String({
+        required: true,
+        description: 'Identificador do interlocutor'
+      }),
+      flgSource: Type.Boolean({
+        required: true,
+        description: 'Indica se o interlocutor é a fonte principal'
+      }),
+      identity: Type.String({
+        required: true,
+        description: 'Identidade do interlocutor'
+      }),
+      chatIdentity: Type.String({
+        required: true,
+        description: 'Identidade do chat do interlocutor'
+      })
+    },
+    { description: 'quem está interagindo com o serviço/agente' }
+  ),
+  attendant: Type.Object(
+    {
+      type: Type.String({
+        required: true,
+        description: 'Tipo do atendente'
+      }),
+      id: Type.String({
+        required: true,
+        description: 'Identificador do atendente'
+      }),
+      flgSource: Type.Boolean({
+        required: true,
+        description: 'Indica se o atendente é a fonte principal'
+      }),
+      identity: Type.String({
+        required: true,
+        description: 'Identidade do atendente'
+      })
+    },
+    { description: 'quando já tem o agente envolvido na chamada' }
+  ),
+  flgConsult: Type.Boolean({
+    description: 'indica se a interação é uma consulta',
+    default: false
+  }),
+  flgIncoming: Type.Boolean({
     required: true,
-    default: new Date()
+    description: 'indica a direção, true é Entrante, false é Sainte',
+    default: true
   }),
-  status: Type.String({
-    required: true,
-    default: callStatus.received,
-    enum: Type.Enum(callStatus)
+  associatedData: Type.String({
+    description: 'dados associados da chamada'
   }),
-  caller: Type.Object({
-    id: Type.String(),
-    name: Type.String(),
-    phoneNumber: Type.String()
+  protocol: Type.String({
+    description: 'Protocolo da chamada'
   }),
-  agent: Type.Object({
-    id: Type.String(),
-    name: Type.String()
+  callbackId: Type.String({
+    description: 'id do contato do callback, indica que foi uma chamada gerada pelo callback'
   }),
-  duration: Type.Number(),
-  notes: Type.String()
+  flgMonitoring: Type.String({
+    description: 'indica se a chamada está sendo monitorada enviando qual ramal está sendo monitorado'
+  }),
+  queuePosition: Type.Optional(
+    Type.Number({
+      description: 'registra a posição que o interlocutor estava na fila (abandono, transbordo)'
+    })
+  ),
+  fidelization: Type.Optional(
+    Type.Boolean({
+      description: 'indica se a chamada é de fidelização'
+    })
+  ),
+  flgPickUp: Type.Optional(
+    Type.Boolean({
+      description: 'indica se o agente atendeu a chamada'
+    })
+  ),
+  flgRecord: Type.Optional(
+    Type.Boolean({
+      description: 'indica se a interação foi gravada'
+    })
+  ),
+  tokenAi: Type.Optional(
+    Type.String({
+      description: 'uuid com token referente à integração com IA no atendimento'
+    })
+  ),
+  hookBy: Type.Optional(
+    Type.String({
+      description: 'quem desligou a chamada (A, B ou I)',
+      enum: hookBy
+    })
+  ),
+  callbackRegState: Type.Optional(
+    Type.String({
+      description: 'status do registro do callback',
+      enum: callbackRegState
+    })
+  ),
+  endReason: Type.Optional(
+    Type.String({
+      description: 'condição de término da interação',
+      enum: endReason
+    })
+  ),
+  causedBy: Type.Optional(
+    Type.String({
+      description: 'causa da condição de término da interação',
+      enum: causedBy
+    })
+  )
 }
 
 export const CallEventValidation = { schema, composition: Type.Object(schema) }
