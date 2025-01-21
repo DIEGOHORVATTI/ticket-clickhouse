217cd882bb8a1f148456cde3bf5ea83833d7de5f
DIEGOHORVATTI <d.horvattid@gmail.com>
2025-01-21 11:17:47 -0300

    Remove jwt-settings module and update shared index exports

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,7 +1,5 @@
 export * from './connection-db'
 
-export * from './jwt-settings'
-
 export * from './pagination-settings'
 export * from './convert-object-to-query'
 export * from './generate-filter-schema'
